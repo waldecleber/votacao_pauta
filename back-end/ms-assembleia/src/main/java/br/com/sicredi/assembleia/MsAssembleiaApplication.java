2e7a86f9b6a8a1bad0812f6c1a4a587e5966a03d
Waldecleber <waldecleber@gmail.com>
2020-05-12 12:15:05 -0300

    Commit inicial

---
+++ back-end/ms-assembleia/src/main/java/br/com/sicredi/assembleia/MsAssembleiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAssembleiaApplication.class, args);
+	}
+
+}
---
+++ back-end/ms-assembleia/src/test/java/br/com/sicredi/assembleia/MsAssembleiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.assembleia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAssembleiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
